b04c04a8dfd12fc5f0bde4c0e4e2cbca0149a8e2
edensmt <epochws@gmail.com>
2019-10-03 12:36:28 -0600

    video player active

---
+++ app/src/main/java/Config.java
@@ -0,0 +1,10 @@
+package com.example.rickrollapi;
+
+public final class Config {
+
+	private Config() {
+	}
+
+	public static final String YOUTUBE_API_KEY = "AIzaSyAoyQ0mNBpoDq-FxjwPhtXQUmqrf0oQRQQ";
+
+}
